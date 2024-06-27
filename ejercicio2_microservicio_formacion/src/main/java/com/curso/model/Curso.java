2195035611417891b6dd9c8b4046714830ed8418
Erick Núñez <ericknn@gmail.com>
2024-06-27 16:08:48 -0600

    Ejercicio 02 Comunicacion entre Microservicios

---
+++ ejercicio2_microservicio_formacion/src/main/java/com/curso/model/Curso.java
@@ -0,0 +1,62 @@
+package com.curso.model;
+
+import java.math.BigDecimal;
+
+
+public class Curso {
+	
+	private String codCurso;
+	
+	private String nombre;
+	
+	private Integer duracion;
+	
+	private BigDecimal precio;
+
+	public Curso() {
+		super();
+	}
+
+	public Curso(String codCurso, String nombre, Integer duracion, BigDecimal precio) {
+		super();
+		this.codCurso = codCurso;
+		this.nombre = nombre;
+		this.duracion = duracion;
+		this.precio = precio;
+	}
+
+	public String getCodCurso() {
+		return codCurso;
+	}
+
+	public void setCodCurso(String codCurso) {
+		this.codCurso = codCurso;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(Integer duracion) {
+		this.duracion = duracion;
+	}
+
+	public BigDecimal getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(BigDecimal precio) {
+		this.precio = precio;
+	}
+	
+	
+
+}
